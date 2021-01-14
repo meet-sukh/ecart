45a5ebaf1c1e5c29b31518f7c571b4c1fe78d4fb
meet-sukh <sk92.saini@gmail.com>
2021-01-14 10:40:19 +0530

    Revert "overridng creds"

--- src/main/java/io/ecart/order/controller/OrderController.java
+++ src/main/java/io/ecart/order/controller/OrderController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,11 +36,4 @@ public class OrderController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(createOrderResponse);
 	}
 
-	@ApiResponses(value = {
-			@ApiResponse(code = 201, response = CreateOrderResponse.class, message = "order deleted") })
-	@DeleteMapping
-	public ResponseEntity<?> deleteOrder(@Valid @RequestBody CreateOrderRequest createOrderRequest) {
-		log.info("deleteOrder for {}");
-		return ResponseEntity.status(HttpStatus.OK).build();
-	}
 }
